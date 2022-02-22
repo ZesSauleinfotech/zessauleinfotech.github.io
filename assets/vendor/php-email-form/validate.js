a34e8450455123220903b51a7137712ad102c2cd
pritkathrotiya <pritkathrotiya@github.com>
2022-02-22 12:35:06 +0530

    update email

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -10,11 +10,11 @@
     e.preventDefault();
     
     var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    ferror = false,
+    emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
-     
+
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -30,35 +30,35 @@
 
         switch (rule) {
           case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
+          if (i.val() === '') {
+            ferror = ierror = true;
+          }
+          break;
 
           case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
+          if (i.val().length < parseInt(exp)) {
+            ferror = ierror = true;
+          }
+          break;
 
           case 'email':
-            if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
+          if (!emailExp.test(i.val())) {
+            ferror = ierror = true;
+          }
+          break;
 
           case 'checked':
-            if (! i.is(':checked')) {
-              ferror = ierror = true;
-            }
-            break;
+          if (! i.is(':checked')) {
+            ferror = ierror = true;
+          }
+          break;
 
           case 'regexp':
-            exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
+          exp = new RegExp(exp);
+          if (!exp.test(i.val())) {
+            ferror = ierror = true;
+          }
+          break;
         }
         i.next('.validate').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
@@ -80,16 +80,16 @@
 
         switch (rule) {
           case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
+          if (i.val() === '') {
+            ferror = ierror = true;
+          }
+          break;
 
           case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
+          if (i.val().length < parseInt(exp)) {
+            ferror = ierror = true;
+          }
+          break;
         }
         i.next('.validate').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
@@ -124,42 +124,71 @@
   });
 
   function php_email_form_submit(this_form, action, data) {
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: data,
-      timeout: 40000
-    }).done( function(msg){
-      if (msg.trim() == 'OK') {
-        this_form.find('.loading').slideUp();
-        this_form.find('.sent-message').slideDown();
-        this_form.find("input:not(input[type=submit]), textarea").val('');
-      } else {
-        this_form.find('.loading').slideUp();
-        if(!msg) {
-          msg = 'Form submission failed and no error message returned from: ' + action + '<br>';
-        }
-        this_form.find('.error-message').slideDown().html(msg);
-      }
-    }).fail( function(data){
-      console.log(data);
-      var error_msg = "Form submission failed!<br>";
-      if(data.statusText || data.status) {
-        error_msg += 'Status:';
-        if(data.statusText) {
-          error_msg += ' ' + data.statusText;
-        }
-        if(data.status) {
-          error_msg += ' ' + data.status;
-        }
-        error_msg += '<br>';
-      }
-      if(data.responseText) {
-        error_msg += data.responseText;
-      }
-      this_form.find('.loading').slideUp();
-      this_form.find('.error-message').slideDown().html(error_msg);
-    });
-  }
+    var form_id_js = "javascript_form";
 
-})(jQuery);
+    var data_js = {
+      "access_token": "yyxl1k4y0r6wsqle7isztw7b"
+    };
+
+    function js_onSuccess() {
+        // remove this to avoid redirect
+        document.querySelector("#" + form_id_js + " [name='subject']").value="";
+        document.querySelector("#" + form_id_js + " [name='text']").value="";
+        document.querySelector("#" + form_id_js + " [name='name']").value="";
+        sendButton.innerHTML='Send Message';
+        document.getElementById('success').style.display="block"; 
+        //window.location = window.location.pathname + "?message=Email+Successfully+Sent%21&isError=0";
+      }
+
+      function js_onError(error) {
+        // remove this to avoid redirect
+        //window.location = window.location.pathname + "?message=Email+could+not+be+sent.&isError=1";
+      }
+
+      var sendButton = document.getElementById("js_send");
+
+      function js_send() {
+        sendButton.innerHTML='Sending…';
+        sendButton.disabled=true;
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+          if (request.readyState == 4 && request.status == 200) {
+            js_onSuccess();
+          } else
+          if(request.readyState == 4) {
+            js_onError(request.response);
+          }
+        };
+
+        var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
+        var message = document.querySelector("#" + form_id_js + " [name='text']").value;
+        data_js['subject'] = subject;
+        data_js['text'] = message;
+        var params = toParams(data_js);
+
+        request.open("POST", "https://postmail.invotes.com/send", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        request.send(params);
+
+        return false;
+      }
+
+      sendButton.onclick = js_send;
+
+      function toParams(data_js) {
+        var form_data = [];
+        for ( var key in data_js ) {
+          form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
+        }
+
+        return form_data.join("&");
+      }
+
+      var js_form = document.getElementById(form_id_js);
+      js_form.addEventListener("submit", function (e) {
+        e.preventDefault();
+      });
+    }
+
+  })(jQuery);
